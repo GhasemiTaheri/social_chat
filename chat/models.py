e87c3bdaf5ab25d878737dae1be34f291694db59
Mojtaba <mghasemi13786@gmail.com>
2024-05-11 13:26:24 +0330

    update models

--- chat/models.py
+++ chat/models.py
@@ -1,67 +1,40 @@
-import random
+from django.contrib.auth import get_user_model
 from django.db import models
-from user.models import User
 
 
-def code_generator():
-    data = "abcdefghijklmnopqrstuvwxyz123456789"
-    result = ""
-    for _ in range(15):
-        result += data[random.randint(0, (len(data) - 1))]
-    return result
+class Group:
+    pass
 
 
-class Group(models.Model):
-    name = models.CharField(max_length=50)
-    unique_id = models.CharField(max_length=15, unique=True, default=code_generator)
-    member = models.ManyToManyField(to=User, related_name="group_member")
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="group_creator")
-    avatar = models.ImageField(upload_to='group_avatar/', blank=True, null=True)
+class Conversation(models.Model):
+    title = models.CharField(max_length=100)
+    creator = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
-    def member_count(self):
-        return self.member.count()
 
-    def last_message(self):
-        m = Message.objects.filter(to_group_id=self.id).last()
-        if m:
-            return m.serializer()
+class Participant(models.Model):
+    SINGLE = "si"
+    GROUP = "gr"
 
-    def serializer(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "unique_id": self.unique_id,
-            "member": [user.serializer() for user in self.member.all()],
-            "owner": self.owner.username,
-            "avatar": self.avatar.name,
-            "member_count": self.member_count(),
-            "last_message": self.last_message(),
-        }
+    CHAT_TYPES = [
+        (SINGLE, "Single"),
+        (GROUP, "Group")
+    ]
 
-    def __str__(self):
-        return f'{self.name} + {self.unique_id}'
+    user = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=0)
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    chat_type = models.CharField(max_length=2, choices=CHAT_TYPES)
+    added_at = models.DateTimeField(auto_now_add=True)
+    last_read = models.DateTimeField(null=True, blank=True)
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="message_sender")
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)
-    to_group = models.ForeignKey(to=Group, null=True, blank=True, on_delete=models.CASCADE,
-                                 related_name="message_to_group")
-    to_user = models.ForeignKey(to=User, null=True, blank=True, on_delete=models.CASCADE,
-                                related_name="message_to_user")
-
-    def letter_count(self):
-        return len(self.text)
-
-    def serializer(self):
-        return {
-            'sender': self.sender.serializer(),
-            'text': self.text,
-            'create_at': self.create_at,
-            'to_group': self.to_group.unique_id,
-            'to_user': self.to_user
-        }
 
     def __str__(self):
         return self.text
--- user/models.py
+++ user/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# Create your models here.
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='user_avatar/', blank=True, null=True)
     bio = models.TextField(max_length=200, null=True, blank=True)
