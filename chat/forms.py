bb02915580b11f071d6bc107ec9742379d4d30cf
Mojtaba <mghasemi13786@gmail.com>
2024-05-12 15:35:57 +0330

    add create group form

--- chat/forms.py
+++ chat/forms.py
@@ -1,3 +1,19 @@
 from django.forms import ModelForm
 
+from chat.models import Conversation
 
+
+class ConversationCreateForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request")
+        super(ConversationCreateForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = Conversation
+        fields = ('title',)
+
+    def save(self, commit=True):
+        instance: Conversation = super().save(commit=False)
+        instance.creator = self.request.user
+        instance.conversation_type = Conversation.GROUP
+        return instance
--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from chat.forms import ConversationCreateForm
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'chat/dashboard.html'
+
+
+class GroupCreateView(LoginRequiredMixin, CreateView):
+    form_class = ConversationCreateForm
+    template_name = 'chat/create_group.html'
+    success_url = reverse_lazy('chat:dashboard')
+
+    def get_form_kwargs(self):
+        kwargs = super(GroupCreateView, self).get_form_kwargs()
+        kwargs.update({'request': self.request})
+        return kwargs
